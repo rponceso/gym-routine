6911076ae1585068237d79aed47ffb91b361b9ff
renatom <renato@gmail.com>
2025-06-02 18:19:06 -0500

    Adicionando cambios

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAdida48PrhOOMvaF_nzQIRfwoy8_tpBqU",
-  authDomain: "gym-routine-73dc9.firebaseapp.com",
-  projectId: "gym-routine-73dc9",
-  storageBucket: "gym-routine-73dc9.firebasestorage.app",
-  messagingSenderId: "423511526411",
-  appId: "1:423511526411:web:a793cfe44e88c6d84ecb0e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
